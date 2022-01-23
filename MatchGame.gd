7c7a3363fa0f741e2e5938bde92ea8ba6f992c50
grimmr <grimms767@gmail.com>
2022-01-23 18:02:55 +0000

    fixed fill

--- MatchGame.gd
+++ MatchGame.gd
@@ -11,37 +11,28 @@ var board = []
 
 enum direction {H, V}
 
-enum {CLEAR, LIFT, FILL}
-var state = CLEAR
+
+var dummy
 
 func _ready():
 	rng.randomize()
+	dummy = WaterScene.instance()
+	dummy.setType(999)
 	setupBoard()
 	print(findMatches())
 
 var count = 0	
 func _process(delta):
-	count += delta
-	if count >= 1 && count <= 10:
-		if(state == CLEAR):
-			state = LIFT
-			count = 0
-			clearAllMatches()
-		elif(state == LIFT):
-			HandleLiftPieces()
-			state = FILL
-			count = 0
-		elif(state == FILL):
-			HandleFill()
-			state = CLEAR
-			count = 0
+	HandleLiftPieces()
+	clearAllMatches()
+
 
 func setupBoard():
 	for x in range(width):
 		board.append([])
 		for y in range(height):
 			board[x].append(null)
-			createWater(x,y)
+			createWater(x, y, y)
 
 func findMatches():
 	var out = []
@@ -50,22 +41,28 @@ func findMatches():
 		var last = -1
 		var start = Vector2(x, 0)
 		for y in range(height+1):
-			if y == height || board[x][y].getType() != last:
-				if(y - start.y >= 3):
+			if y == height || board[x][y] == null || board[x][y].getType() != last:
+				if(y - start.y >= 3 && last != 999):
 					out.append(matchData.new(direction.V, start, Vector2(x, y-1)))
-				if(y < height):
-					last = board[x][y].getType()
+				elif(y < height):
+					if(board[x][y] == null):
+						last = -1
+					else:
+						last = board[x][y].getType()
 				start = Vector2(x, y)
 	#horizontal
 	for y in range(height):
 		var last = -1
 		var start = Vector2(0, y)
 		for x in range(width+1):
-			if x == width || board[x][y].getType() != last:
-				if(x - start.x >= 3):
+			if x == width || board[x][y] == null || board[x][y].getType() != last:
+				if(x - start.x >= 3 && last != 999):
 					out.append(matchData.new(direction.H, start, Vector2(x-1, y)))
 				if x < width:
-					last = board[x][y].getType()
+					if(board[x][y] == null):
+						last = -1
+					else:
+						last = board[x][y].getType()
 				start = Vector2(x, y)
 	return out 
 
@@ -77,40 +74,45 @@ func clearAllMatches():
 		for v in range(start, end+1):
 			remove(m.start.x if m.type==direction.V else v, m.start.y if m.type==direction.H else v)
 
-func shiftColumnOne(col, start):
-	for y in range(start+1, height):
-		var moving = board[col][y]
-		board[col][y-1] = moving
-		board[col][y] = null
-		if moving != null:
-			moving.moveTo(col*gridSize + .5*gridSize, (y-1)*gridSize + .5*gridSize)
-
 func HandleLiftPieces():
 	for x in range(width):
-		for y in range(height-1, -1, -1):
-			if(board[x][y] == null):
-				shiftColumnOne(x, y)
+		#lift
+		for y in range(1, height):
+			if(board[x][y-1] == null && board[x][y] != null && board[x][y] != dummy):
+				moveTileBy(board[x][y], 0, -1)
+				board[x][y] = null
+		#new
+		if board[x][height-1] == null:
+			createWater(x, height-1, 0)
 
-func createWater(x, y):
+func createWater(x, y, offset = 0):
 	var newWater = WaterScene.instance()
-	newWater.setType(rng.randi_range(0,3))
-	newWater.position.x = x*gridSize + .5*gridSize
-	newWater.position.y = y*gridSize + .5*gridSize
+	newWater.setType(rng.randi_range(0,4))
+	newWater.position.x = x*gridSize 
+	newWater.position.y = (height+offset)*gridSize
+	newWater.connect("arrived", self, "onArival")
 	add_child(newWater)
-	board[x][y] = newWater
-
-func HandleFill():
-	for x in range(width):
-		var start = false
-		for y in range(height):
-			if board[x][y] == null:
-				start = true
-			if start:
-				createWater(x,y)
+	moveTileTo(newWater, x, y)
+	return newWater
+			
 
 func remove(x, y):
 	remove_child(board[x][y])
 	board[x][y] = null
+	
+func swapPieceRight(target):
+	if(target.x < 0 || target.x >= width-1 || target.y < 0 || target.y >= height):
+		return false
+	if(board[target.x+1][target.y] == null || board[target.x][target.y] == null):
+		return false
+	if(board[target.x+1][target.y].getType() == 999 || board[target.x][target.y].getType() == 999):
+		return false
+	board[target.x+1][target.y].moveBy(-gridSize, 0)
+	board[target.x][target.y].moveBy(gridSize, 0)
+	#set dummies
+	board[target.x][target.y] = dummy
+	board[target.x+1][target.y] = dummy
+	return true
 
 class matchData:
 	var type
@@ -123,3 +125,29 @@ class matchData:
 	
 	func _to_string():
 		return "|" + ("V" if type==direction.V else "H") + ", start: %s, end:%s|" % [start,end] 
+
+func moveTileTo(tile, x, y):
+	if(tile.getType() == 999):
+		return
+	var gridPos = tile.position / gridSize
+	if gridPos.x < width && gridPos.x >= 0 && gridPos.y < height && gridPos.y >= 0:
+		board[gridPos.x][gridPos.y] = dummy
+	board[x][y] = dummy
+	tile.moveTo(x*gridSize, y*gridSize)
+
+func moveTileBy(tile, x, y):
+	var gridPos = tile.position / gridSize
+	moveTileTo(tile, gridPos.x + x, gridPos.y + y)
+
+func onArival(tile, from, to):
+	var location = to/gridSize
+	board[location.x][location.y] = tile
+	
+
+func _input(event):
+	if event.is_action_pressed("gemGame_Swap"):
+		swapPieceRight(get_local_mouse_position() / gridSize)
+	if event.is_action("debug"):
+		print("-------------------")
+		print(board)
+		print(findMatches())
